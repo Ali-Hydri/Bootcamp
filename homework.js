c3972d2b20a60d3e6afdcbb8013404850c3f1b5a
Ali Heydari <ali@Alis-MacBook-Pro.local>
2024-08-07 00:50:19 +0330

    final js homework

--- homework.js
+++ homework.js
@@ -1,26 +1,25 @@
-function greetUser(gender) {
-    if (gender === 'man') {
-        console.log('خوش آمدید آقا');
-    } else if (gender === 'woman') {
-        console.log('خوش آمدید خانم');
+function greeting(gender) {
+    if (gender == "male") {
+      console.log("wellcome sir");
     } else {
-        console.log('جنسیت نامشخص است');
+      console.log("wellcome mam");
     }
-}
-
-
-
-
-function greetBasedOnTime(hour) {
-    if (hour >= 0 && hour < 12) {
-        console.log('صبح بخیر');
-    } else if (hour >= 12 && hour < 17) {
-        console.log('ظهر بخیر');
-    } else if (hour >= 17 && hour < 21) {
-        console.log('عصر بخیر');
-    } else if (hour >= 21 && hour <= 23) {
-        console.log('شب بخیر');
+  }
+  
+  let jensiat = "male";
+  greeting(jensiat);
+  
+  // ###########################
+  
+  function greetingByTime(time) {
+    if (time <= 10) {
+      console.log("good morning");
+    } else if (time <= 18) {
+      console.log("good afternoon");
     } else {
-        console.log('ساعت نامعتبر است');
+      console.log("good night");
     }
-}
+  }
+  let time = 15
+  greetingByTime(time);
+  

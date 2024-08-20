d611f3d820ecf40bb48c80bc8fde316088f64560
Ali Heydari <ali@Alis-MacBook-Pro.local>
2024-08-21 00:18:59 +0330

    homework-s06

---
+++ homework-session06.js
@@ -0,0 +1,39 @@
+
+for (let i = 0; i < 1; i++) {
+    let row = "";
+    for (let j = 0; j < 1; j++) {
+      row += " ***** ";
+    }
+    console.log(row);
+  }
+  
+  let row = "";
+  for (let j = 0; j < 1; j++) {
+    row += "*     *";
+  }
+  console.log(row);
+  
+  for (let z = 0; z < 1; z++) {
+    row = "* * * *";
+  }
+  console.log(row);
+  
+  for (let z = 0; z < 1; z++) {
+    row = "*  *  *";
+  }
+  console.log(row);
+  
+  for (let z = 0; z < 1; z++) {
+    row = "* *** *";
+  }
+  console.log(row);
+  
+  for (let z = 0; z < 1; z++) {
+    row = "*     *";
+  }
+  console.log(row);
+  
+  for (let z = 0; z < 1; z++) {
+    row = " ***** ";
+  }
+  console.log(row);
